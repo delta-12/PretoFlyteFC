c11934040c0a777160b4916deab65a1c534edd9f
delta-12 <btesq235@gmail.com>
2023-12-12 23:34:08 -0500

    test Kalman filter and pitch correction

--- main/PretoFlyteFC.c
+++ main/PretoFlyteFC.c
@@ -5,6 +5,7 @@
 #include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "Kalman.h"
 #include "Lsm9ds1.h"
 #include "math.h"
 #include "Sbus.h"
@@ -42,11 +43,15 @@ static Sbus_Handle_t PretoFlyteFC_SbusHandle;
 static uint16_t PretoFlyteFC_SbusChannelBuffer[SBUS_CHANNELS] = {0U};
 static Sbus_Payload_t PretoFlyteFC_SbusPayload;
 
+static Kalman_1dFilterContext_t PretoFlyteFC_PitchFilter;
+static Kalman_1dFilterContext_t PretoFlyteFC_RollFilter;
+
 /* Function Prototypes
  ******************************************************************************/
 
 static void PretoFlyteFC_LogAccelGyroTask(void *arg);
-static uint32_t PretoFlyteFC_Millis(void);
+static inline int64_t PretoFlyteFC_Micros(void);
+static inline int64_t PretoFlyteFC_Millis(void);
 
 /* Function Definitions
  ******************************************************************************/
@@ -64,12 +69,18 @@ void app_main(void)
         .IntM = GPIO_NUM_27,
         .Rdy = GPIO_NUM_15,
     };
-    Lsm9ds1_Init(&PretoFlyteFC_Lsm9ds1Handle, SPI3_HOST, &lsm9ds1PinConfig);
-    Lsm9ds1_GyroInit(&PretoFlyteFC_Lsm9ds1Handle);
+    if (!Lsm9ds1_Init(&PretoFlyteFC_Lsm9ds1Handle, SPI3_HOST, &lsm9ds1PinConfig))
+    {
+        /* Dead loop if IMU failed to initialize */
+        while (1u)
+        {
+            vTaskDelay(10U / portTICK_PERIOD_MS);
+        }
+    }
     Lsm9ds1_AccelInit(&PretoFlyteFC_Lsm9ds1Handle);
-    Lsm9ds1_MagInit(&PretoFlyteFC_Lsm9ds1Handle);
-    // Lsm9ds1_AccelGyroCalibrate(&PretoFlyteFC_Lsm9ds1Handle);
-    // xTaskCreate(PretoFlyteFC_LogAccelGyroTask, "PretoFlyteFC_LogAccelGyroTask", 2048U, NULL, 10U, NULL);
+    Lsm9ds1_GyroInit(&PretoFlyteFC_Lsm9ds1Handle);
+    Lsm9ds1_AccelGyroCalibrate(&PretoFlyteFC_Lsm9ds1Handle);
+    xTaskCreate(PretoFlyteFC_LogAccelGyroTask, "PretoFlyteFC_LogAccelGyroTask", 2048U, NULL, 10U, NULL);
 
     Sbus_InitHandle(&PretoFlyteFC_SbusHandle, SBUS_UART, SBUS_RX_PIN, SBUS_TX_PIN, false, &PretoFlyteFC_SbusRxBuffer, &PretoFlyteFC_SbusTxBuffer);
     Sbus_InitPayload(&PretoFlyteFC_SbusPayload, &PretoFlyteFC_SbusChannelBuffer);
@@ -82,7 +93,7 @@ void app_main(void)
     Sbus_SetChannel(&PretoFlyteFC_SbusPayload, SBUS_AUX_1_CHANNEL, SBUS_CHANNEL_VALUE(0.00));
     Sbus_SetChannel(&PretoFlyteFC_SbusPayload, SBUS_AUX_2_CHANNEL, SBUS_CHANNEL_VALUE(1.00));
 
-    uint32_t endTime = PretoFlyteFC_Millis() + 10000U;
+    int64_t endTime = PretoFlyteFC_Millis() + 10000U;
     while (PretoFlyteFC_Millis() < endTime)
     {
         Sbus_Tx(&PretoFlyteFC_SbusHandle, &PretoFlyteFC_SbusPayload);
@@ -138,9 +149,18 @@ static void PretoFlyteFC_LogAccelGyroTask(void *arg)
 {
     double pitch;
     double roll;
+    double pitchRate;
+    double rollRate;
+    double sampleTime = 0;
+    int64_t previousSampleTime = 0;
+    int64_t currentTime = 0;
+
+    Kalman_1dFilterInit(&PretoFlyteFC_PitchFilter, 0, 4, 4, 3);
+    Kalman_1dFilterInit(&PretoFlyteFC_RollFilter, 0, 4, 4, 3);
 
     for (;;)
     {
+        /* Read accelerometer and gyroscope */
         if (Lsm9ds1_AccelAvailable(&PretoFlyteFC_Lsm9ds1Handle))
         {
             Lsm9ds1_AccelRead(&PretoFlyteFC_Lsm9ds1Handle);
@@ -150,13 +170,20 @@ static void PretoFlyteFC_LogAccelGyroTask(void *arg)
             Lsm9ds1_GyroRead(&PretoFlyteFC_Lsm9ds1Handle);
         }
 
+        /* Calculate and update sample time */
+        currentTime = PretoFlyteFC_Micros();
+        sampleTime = (double)(currentTime - previousSampleTime) * 0.000001;
+        previousSampleTime = currentTime;
+
         /* Pitch = atan(-ax, sqrt(ay^2, az^2)) */
         pitch = atan2(PretoFlyteFC_Lsm9ds1Handle.Accel.Reading.X * -1.0,
                       sqrt(PretoFlyteFC_Lsm9ds1Handle.Accel.Reading.Y * PretoFlyteFC_Lsm9ds1Handle.Accel.Reading.Y +
                            PretoFlyteFC_Lsm9ds1Handle.Accel.Reading.Z * PretoFlyteFC_Lsm9ds1Handle.Accel.Reading.Z));
+        pitchRate = PretoFlyteFC_Lsm9ds1Handle.Gyro.Reading.Y;
 
         /* Roll = atan2(ay, az) */
         roll = atan2(PretoFlyteFC_Lsm9ds1Handle.Accel.Reading.Y, PretoFlyteFC_Lsm9ds1Handle.Accel.Reading.Z);
+        rollRate = PretoFlyteFC_Lsm9ds1Handle.Gyro.Reading.X;
 
         ESP_LOGI(PretoFlyteFC_LogTag, "Ax: %.02lf, Ay: %.02lf, Az: %.02lf",
                  PretoFlyteFC_Lsm9ds1Handle.Accel.Reading.X,
@@ -170,15 +197,28 @@ static void PretoFlyteFC_LogAccelGyroTask(void *arg)
         /* Convert from radians to degrees */
         pitch *= 180.0 / M_PI;
         roll *= 180.0 / M_PI;
-        ESP_LOGI(PretoFlyteFC_LogTag, "Pitch: %.02lf deg, Roll: %.02lf deg", pitch, roll);
+        ESP_LOGI(PretoFlyteFC_LogTag, "Pitch[Raw]: %.02lf deg, Roll[Raw]: %.02lf deg", pitch, roll);
 
-        vTaskDelay(250U / portTICK_PERIOD_MS);
+        /* Correct pitch? */
+        pitch -= 1.6;
+
+        /* Apply 1D Kalman filter to pitch and roll */
+        Kalman_1dFilter(&PretoFlyteFC_PitchFilter, pitchRate, pitch, sampleTime);
+        Kalman_1dFilter(&PretoFlyteFC_RollFilter, rollRate, roll, sampleTime);
+        ESP_LOGI(PretoFlyteFC_LogTag, "Pitch[Filter]: %.02lf deg, Roll[Filter]: %.02lf deg", PretoFlyteFC_PitchFilter.State, PretoFlyteFC_RollFilter.State);
+
+        vTaskDelay(10U / portTICK_PERIOD_MS);
     }
 
     vTaskDelete(NULL);
 }
 
-static uint32_t PretoFlyteFC_Millis(void)
+static inline int64_t PretoFlyteFC_Micros(void)
 {
-    return (uint32_t)(esp_timer_get_time() / US_PER_MS);
+    return esp_timer_get_time();
+}
+
+static inline int64_t PretoFlyteFC_Millis(void)
+{
+    return (int64_t)(PretoFlyteFC_Micros() / US_PER_MS);
 }
